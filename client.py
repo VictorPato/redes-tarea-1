215d71d6e39a540f9f7d96b9450f8e98cd705d8e
VictorPato <victor.garrido.u@gmail.com>
2018-10-01 21:14:36 -0300

    agrega puerto 23 por default

--- client.py
+++ client.py
@@ -8,21 +8,25 @@ clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # nombre del server
 nombre = sys.argv[1]
 
-# elejimos el puerto a conectar
-puerto = int(sys.argv[2])
+puerto = 23
+
+if len(sys.argv) == 3:
+    # elejimos el puerto a conectar
+    puerto = int(sys.argv[2])
+
+print("Conectando a "+nombre+" en el puerto " + str(puerto))
 
 # lo conectamos al puerto acordado
 clientsocket.connect(('localhost', puerto))
-print("Conectando a "+nombre+" en el puerto " + str(puerto))
-first_answer = clientsocket.recv(2048)
 
+first_answer = clientsocket.recv(2048)
 print(first_answer.decode(), end='')
 
 while True:
     message = input()
+    clientsocket.send(message.encode())
     if message.strip() == 'exit':
         break
-    clientsocket.send(message.encode())
     answer = clientsocket.recv(2048)
     print(answer.decode(), end='')
 
